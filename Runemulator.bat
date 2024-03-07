8cdcaa78c729cb489585e533f6243300df615f1c
Alfredo Ponce Zárate <alfredoponcezarate@gmail.com>
2024-03-06 21:15:47 -0600

    06-03-2004

---
+++ Runemulator.bat
@@ -0,0 +1,2 @@
+powershell.exe -ExecutionPolicy Bypass -File "C:\Users\Alfredo\Documents\DiscoC\Areas\Area de Empresas\Jesus\invoiceSnap\emulator.ps1"
+pause
---
+++ emulator.ps1
@@ -0,0 +1,3 @@
+cd C:\Users\Alfredo\AppData\Local\Android\sdk\emulator\
+.\emulator -avd Pixel_6_Pro_API_30 -verbose -debug all -show-kernel -screen touch
+Read-Host -Prompt "Presiona Enter para salir"
